153cca3411e23d245c73858b46d490df494614fd
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-07-03 03:40:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
+mkdir ~/.dotfiles
+
+# Zsh
+sudo apt install zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.dotfiles/.oh-my-zsh"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.dotfiles/.zsh-syntax-highlighting" --depth 1
+
+
+# Vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl 'https://raw.githubusercontent.com/rishabhdeepsingh/.dotfiles/master/.vimrc' -o ~/.vimrc
+
+# Bashrc
+curl 'https://raw.githubusercontent.com/rishabhdeepsingh/.dotfiles/master/.bashrc' -o ~/.bashrc
