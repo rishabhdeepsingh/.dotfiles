3dae0581b8c1e7c85b36e1f4f2df31676e5e92b8
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-11-26 21:17:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ mkdir ~/.dotfiles
 # Zsh
 sudo apt install zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.dotfiles/.oh-my-zsh"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.dotfiles/.zsh-syntax-highlighting" --depth 1
+curl 'https://raw.githubusercontent.com/rishabhdeepsingh/.dotfiles/master/.zshrc' -o ~/.zshrc
 
 
 # Vim
