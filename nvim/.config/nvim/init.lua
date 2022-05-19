377955298fe0dfb11af094fc34a9b8e013507a5b
rishabhdeepsingh <rishabhdeepsingh98@gmail.com>
2022-05-20 03:09:55 +0530

    remove bufferline

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,22 +34,24 @@ require('packer').startup(function()
   use 'marko-cerovac/material.nvim'
   use 'ryanoasis/vim-devicons'
   use 'airblade/vim-current-search-match'
+  use { 'romgrk/barbar.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
+  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
 
   use 'preservim/nerdtree'
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
 
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
+
   -- Add git related info in the signs columns and popups
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-treesitter/playground'
+
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use { 'romgrk/barbar.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
 
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
   use 'saadparwaiz1/cmp_luasnip'
