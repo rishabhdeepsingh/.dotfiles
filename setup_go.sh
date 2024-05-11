b933214ca74e9afb056d1213e973bec22d67d9f8
whiteknife <rishabhdeepsingh98@gmail.com>
2024-05-11 17:37:56 +0530

    Update tmux to TPM and update go verstion

--- setup_go.sh
+++ setup_go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/tomnomnom/dotfiles/blob/master/go-setup.sh
 # Dirty dirty dirty
-VERSION="1.21.0"
+VERSION="1.22.3"
 
 wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
 tar xvfz go$VERSION.linux-amd64.tar.gz
